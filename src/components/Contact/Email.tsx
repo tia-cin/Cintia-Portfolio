6756cd0b83a6faa449ccc494355a4ada6e834ea6
Cintia Arce <cintiaayelenarce@gmail.com>
2022-08-10 12:05:40 -0300

    emailjs done

--- src/components/Contact/Email.tsx
+++ src/components/Contact/Email.tsx
@@ -1,26 +1,49 @@
-import React from "react";
-import { Grid, Button, FormControl, TextField } from "@mui/material";
+import React, { FC } from "react";
+import { Grid, Button, TextField } from "@mui/material";
+// emailjs
+import emailjs from "@emailjs/browser";
 // icons
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 import TitleIcon from "@mui/icons-material/Title";
 import SubjectIcon from "@mui/icons-material/Subject";
 
-export const Email = () => {
+export const Email: FC = () => {
+  const sendMail = (e: any) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_o0smcjb",
+        "template_3pqqcx5",
+        e.target,
+        "Q2GNQFDrDl6zPbgn9"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <Grid id="contact" container>
       <Grid container justifyContent="center">
         <Grid item xs={11}>
           <Grid>Let's get in touch!</Grid>
-          <FormControl>
+          <form onSubmit={sendMail}>
             <Grid container item xs={6}>
               <Grid>Full name</Grid>
             </Grid>
             <Grid>
               <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
               <TextField
+                name="name"
                 id="input-with-sx"
-                label="With sx"
+                label="Your name"
                 variant="standard"
               />
             </Grid>
@@ -32,8 +55,9 @@ export const Email = () => {
                 sx={{ color: "action.active", mr: 1, my: 0.5 }}
               />
               <TextField
+                name="email"
                 id="input-with-sx"
-                label="With sx"
+                label="Your email"
                 variant="standard"
               />
             </Grid>
@@ -43,8 +67,9 @@ export const Email = () => {
             <Grid>
               <TitleIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
               <TextField
+                name="subject"
                 id="input-with-sx"
-                label="With sx"
+                label="Email Subject"
                 variant="standard"
               />
             </Grid>
@@ -56,11 +81,11 @@ export const Email = () => {
               <textarea placeholder="Add your message here" name="message" />
             </Grid>
             <Grid>
-              <Button variant="contained" color="primary">
+              <Button variant="contained" color="primary" type="submit">
                 Send
               </Button>
             </Grid>
-          </FormControl>
+          </form>
         </Grid>
       </Grid>
     </Grid>
