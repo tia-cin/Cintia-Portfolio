946dce07744e0cbac6739301313be78f4413bda7
Cintia Arce <cintiaayelenarce@gmail.com>
2022-08-19 14:44:01 -0300

    profile responsive

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -16,7 +16,7 @@ import blob8 from "../assets/svgs/output-onlinepngtools (8).png";
 export const StyledGrid = styled(Grid)`
   position: relative;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -29,9 +29,27 @@ export const StyledGrid = styled(Grid)`
     left: 0;
     object-fit: contain;
   }
+  @media only screen and (max-width: 600px) {
+    flex-direction: column-reverse;
+    h1 {
+      font-size: 3em;
+    }
+    h2 {
+      font-size: 1.75em;
+    }
+    img {
+      width: 100;
+      height: 100;
+    }
+    .svgs-continer {
+      opacity: 0;
+    }
+  }
 `;
 
 export const Profile: FC = () => {
+  const blobs = [blob1, blob2, blob3, blob4, blob5, blob6, blob7, blob8];
+
   const animation = (e: any) => {
     document.querySelectorAll(".animated").forEach((move: any) => {
       let value = move.getAttribute("id");
@@ -44,89 +62,36 @@ export const Profile: FC = () => {
   document.addEventListener("mousemove", animation);
 
   return (
-    <main style={{ height: "70vh" }}>
-      <StyledGrid container justifyContent="center">
-        <Grid item sx={{ margin: "2em" }}>
-          <Typography variant="h1" className="animated" id="2">
-            Hi, I'm Cintia a
-          </Typography>
-          <Typography variant="h2" className="animated" id="-3">
-            Frontend Developer
-          </Typography>
-        </Grid>
-        <Grid item sx={{ margin: "2em" }}>
-          <Avatar
-            src={profile}
-            className="animated"
-            id="-5"
-            sx={{ width: 200, height: 200, backgroundColor: "#51ABB2" }}
-          />
-        </Grid>
-        <img
-          src={blob1}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="5"
-        />
-        <img
-          src={blob2}
-          alt="svg"
-          width="100"
-          height="100"
+    <StyledGrid container justifyContent="center">
+      <Grid item sx={{ margin: "2em" }}>
+        <Typography variant="h1" className="animated" id="2">
+          Hi, I'm Cintia a
+        </Typography>
+        <Typography variant="h2" className="animated" id="-3">
+          Frontend Developer
+        </Typography>
+      </Grid>
+      <Grid item sx={{ margin: "2em" }}>
+        <Avatar
+          src={profile}
           className="animated"
           id="-5"
+          sx={{ width: 200, height: 200, backgroundColor: "#51ABB2" }}
         />
-        <img
-          src={blob3}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="4"
-        />
-        <img
-          src={blob4}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="-2"
-        />
-        <img
-          src={blob5}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="-2"
-        />
-        <img
-          src={blob6}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="-2"
-        />
-        <img
-          src={blob7}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="1"
-        />
-        <img
-          src={blob8}
-          alt="svg"
-          width="100"
-          height="100"
-          className="animated"
-          id="-1"
-        />
-      </StyledGrid>
-    </main>
+      </Grid>
+      <Grid className="svgs-continer">
+        {blobs.map((b, i) => (
+          <img
+            alt="blob"
+            src={b}
+            key={i}
+            width="100"
+            height="100"
+            className="animated"
+            id={(Math.floor(Math.random() * 4) + 1).toString()}
+          />
+        ))}
+      </Grid>
+    </StyledGrid>
   );
 };
