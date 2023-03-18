533dd4b7136e2735727e1610938dac0b9b6c9aed
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-18 19:40:49 -0300

    projects animation fixes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,23 +11,26 @@ const Projects: React.FC<{
   React.useEffect(() => {
     let sections = gsap.utils.toArray("#card");
 
-    // gsap.to(sections, {
-    //   xPercent: -100 * (sections.length - 1),
-    //   ease: "none",
-    //   scrollTrigger: {
-    //     trigger: "#projects-container",
-    //     pin: true,
-    //     scrub: 1,
-    //     snap: 1 / (sections.length - 1),
-    //     end: "600vh",
-    //   },
-    // });
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#projects-container",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: () =>
+          "+=" +
+          (document.querySelector(".container") as HTMLElement)?.offsetWidth,
+      },
+    });
   }, []);
   return (
     <div
       id="projects-container"
+      className="flex "
       // className="flex flex-col justify-center items-center relative w-full min-h-screen overflow-hidden"
-      style={{ overscrollBehavior: "none" }}
+      style={{ overscrollBehavior: "none", width: "400%" }}
     >
       {projects.map((p: any, i: number) => (
         <Cards
