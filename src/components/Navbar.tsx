c5098ebf86ee0c75ac013ac308044316df4e438f
Cintia Arce <cintiaayelenarce@gmail.com>
2023-03-22 12:30:04 -0300

    navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,93 @@
 import React from "react";
 import { gsap } from "gsap";
+import { BsList, BsChevronLeft } from "react-icons/bs";
 
 const Navbar: React.FC = () => {
+  const [screenSize, setScreenSize] = React.useState<number>(0);
+  const [activeMenu, setActiveMenu] = React.useState<boolean>(false);
+  const [wrap, setWrap] = React.useState<boolean>(false);
+
   React.useEffect(() => {
     gsap.fromTo(
       "nav",
       { opacity: 0, y: -20, duration: 1 },
       { opacity: 1, y: 0, duration: 1 }
     );
+
+    const handleScreenSize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleScreenSize);
+    handleScreenSize();
+    return () => window.removeEventListener("resize", handleScreenSize);
   }, []);
+
+  React.useEffect(() => {
+    if (screenSize <= 900) {
+      setWrap(false);
+    } else {
+      setWrap(true);
+    }
+  }, [screenSize]);
   return (
     <nav
-      className="fixed flex justify-between w-full h-80 top-0 uppercase"
+      className="fixed flex justify-between w-full h-80 top-0 uppercase "
       style={{ zIndex: 1 }}
     >
-      <div className="m-5 ">
+      <div className="m-5">
         <p className="text-xl font-semibold">Portfolio</p>
       </div>
-      <ul className="m-5 flex justify-around w-1/2">
-        <li>
-          <a href="#home">home</a>
-        </li>
-        <li>
-          <a href="#about">about me</a>
-        </li>
-        <li>
-          <a href="#tech-stack">technologies</a>
-        </li>
-        <li>
-          <a href="#design">design</a>
-        </li>
-        <li>
-          <a href="#projects">projects</a>
-        </li>
-        <li>
-          <a href="#contact">contact</a>
-        </li>
-      </ul>
+      {wrap ? (
+        <ul className="m-5 flex justify-around w-1/2">
+          <li>
+            <a href="#home">home</a>
+          </li>
+          <li>
+            <a href="#about">about me</a>
+          </li>
+          <li>
+            <a href="#tech-stack">technologies</a>
+          </li>
+          <li>
+            <a href="#design">design</a>
+          </li>
+          <li>
+            <a href="#projects">projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      ) : activeMenu ? (
+        <ul className="fixed right-0 ml-5 p-5 flex flex-col bg-gray-400 w-2/5 h-screen">
+          <li
+            className="flex items-center font-bold"
+            onClick={() => setActiveMenu(false)}
+          >
+            <BsChevronLeft className="mr-2" /> Go Back
+          </li>
+          <li>
+            <a href="#home">home</a>
+          </li>
+          <li>
+            <a href="#about">about me</a>
+          </li>
+          <li>
+            <a href="#tech-stack">technologies</a>
+          </li>
+          <li>
+            <a href="#design">design</a>
+          </li>
+          <li>
+            <a href="#projects">projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      ) : (
+        <div className="mx-10 m-5 text-xl">
+          <BsList onClick={() => setActiveMenu(true)} />
+        </div>
+      )}
     </nav>
   );
 };
