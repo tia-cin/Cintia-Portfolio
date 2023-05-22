444da889c1b122912fa1452dd3f2e0a11d92be85
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-22 10:15:26 -0300

    navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,7 @@ const items: { [key: string]: string } = {
 };
 
 const Navbar: React.FC = () => {
-  const [screenSize, setScreenSize] = React.useState<number>(0);
   const [activeMenu, setActiveMenu] = React.useState<boolean>(false);
-  const [wrap, setWrap] = React.useState<boolean>(false);
   const [pageSelected, setPageSelected] = React.useState<string>("");
 
   const handlePage = (page: string): void => {
@@ -25,84 +23,43 @@ const Navbar: React.FC = () => {
     setActiveMenu(!activeMenu);
   };
 
-  const NavItems: React.FC<{
-    classes: string;
-    items: { [key: string]: string };
-    child?: React.ReactNode;
-    handlePage: (page: string) => void;
-    pageSelected: string;
-  }> = ({ classes, child, handlePage, pageSelected, items }) => {
-    return (
-      <ul className={classes}>
-        {child}
-        {items &&
-          Object.entries(items).map(([key, value]) => (
-            <li
-              key={key}
-              className={`${pageSelected === key && "text-slate-300"}`}
-              onClick={() => handlePage(key)}
-            >
-              <a href={key}>{value}</a>
-            </li>
-          ))}
-      </ul>
-    );
-  };
-
   React.useEffect(() => {
     gsap.fromTo(
       "nav",
       { opacity: 0, y: -20, duration: 1 },
       { opacity: 1, y: 0, duration: 1 }
     );
-
-    const handleScreenSize = () => setScreenSize(window.innerHeight);
-
-    window.addEventListener("resize", handleScreenSize);
-    handleScreenSize();
-
-    return () => window.removeEventListener("resize", handleScreenSize);
   }, []);
-
-  React.useEffect(() => {
-    if (screenSize <= 900) {
-      setWrap(false);
-    } else {
-      setWrap(true);
-    }
-  }, [screenSize]);
   return (
     <nav
       className={`fixed flex justify-between top-0 w-full uppercase bg-main-bg z-10 ${
-        activeMenu ? "flex-col" : ""
+        activeMenu && "flex-col"
       }`}
     >
       <div className="p-5">
         <p className="text-xl font-semibold">Portfolio</p>
       </div>
-      {wrap ? (
-        <NavItems
-          pageSelected={pageSelected}
-          handlePage={handlePage}
-          items={items}
-          classes={`p-5 flex justify-around w-1/2 ${
-            activeMenu ? "hidden" : ""
-          }`}
-        />
-      ) : activeMenu ? (
-        <NavItems
-          classes="fixed right-0 ml-5 p-5 flex flex-col gap-3 bg-gray-400 w-[220px] h-screen"
-          child={
-            <li className="flex items-center font-bold" onClick={toggleMenu}>
-              <BsChevronLeft className="mr-2" /> Go Back
-            </li>
-          }
-          handlePage={handlePage}
-          pageSelected={pageSelected}
-          items={items}
-        />
+      {activeMenu ? (
+        <ul className="responsive fixed right-0 ml-5 p-5 flex flex-col gap-3 bg-gray-400 h-screen w-1/5 md:w-2/5">
+          <li
+            className="flex items-center font-bold cursor-pointer"
+            onClick={toggleMenu}
+          >
+            <BsChevronLeft className="mr-2" /> Go Back
+          </li>
+          {items &&
+            Object.entries(items).map(([key, value]) => (
+              <li
+                key={key}
+                className={`${pageSelected === key && "text-slate-300"}`}
+                onClick={() => handlePage(key)}
+              >
+                <a href={key}>{value}</a>
+              </li>
+            ))}
+        </ul>
       ) : (
-        <div className=" p-5 text-xl">
+        <div className="p-5 text-3xl cursor-pointer">
           <BsList onClick={toggleMenu} />
         </div>
       )}
